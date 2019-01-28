b80fb0465d3c06f31fae16239f87c1c3797f28bf
Anna Lornitzo <Anna@Annas-Mac.local>
2019-01-28 13:45:07 -0500

    moved fodmaps arr creation out of routes and into separate function

--- controllers/food.js
+++ controllers/food.js
@@ -5,13 +5,13 @@ const Food = require('../models/food_model.js');
 
 router.use(express.static('public'));
 
+// get edit page
 router.get('/:id/edit', (req, res) => {
   Food.findById(req.params.id, (err, selectedItem) => {
     res.render(
       './food_pages/edit.ejs',
       {
         item: selectedItem,
-
       }
     )
   })
@@ -28,103 +28,68 @@ router.get('/', (req, res) => {
   });
 })
 
+// get new page
 router.get('/new', (req, res) => {
   res.render('./food_pages/new.ejs');
 })
 
-router.put('/:id', (req, res) => {
+// create array to push into FODMAPS array of objects to then store in DB.
+const makeFodMapsArr = (item) => {
+  // declare empty array for pushing fodmap objects into
   let fodMapsARR = []
-
+  // make first fodmap object
   let fodmapObj1 = {
-    servingSizeW1: req.body.servingSizeW1,
-    servingSizeV1: req.body.servingSizeV1,
-    oligos1: req.body.oligos1,
-    fructos1: req.body.fructos1,
-    polyols1: req.body.polyols1,
-    lactos1: req.body.lactos1
+    servingSizeW1: item.servingSizeW1,
+    servingSizeV1: item.servingSizeV1,
+    oligos1: item.oligos1,
+    fructos1: item.fructos1,
+    polyols1: item.polyols1,
+    lactos1: item.lactos1
   }
+  // push into arr
   fodMapsARR.push(fodmapObj1);
 
   let fodmapObj2 = {
-    servingSizeW2: req.body.servingSizeW2,
-    servingSizeV2: req.body.servingSizeV2,
-    oligos2: req.body.oligos2,
-    fructos2: req.body.fructos2,
-    polyols2: req.body.polyols2,
-    lactos2: req.body.lactos2
+    servingSizeW2: item.servingSizeW2,
+    servingSizeV2: item.servingSizeV2,
+    oligos2: item.oligos2,
+    fructos2: item.fructos2,
+    polyols2: item.polyols2,
+    lactos2: item.lactos2
   }
   fodMapsARR.push(fodmapObj2);
 
   let fodmapObj3 = {
-    servingSizeW3: req.body.servingSizeW3,
-    servingSizeV3: req.body.servingSizeV3,
-    oligos3: req.body.oligos3,
-    fructos3: req.body.fructos3,
-    polyols3: req.body.polyols3,
-    lactos3: req.body.lactos3
+    servingSizeW3: item.servingSizeW3,
+    servingSizeV3: item.servingSizeV3,
+    oligos3: item.oligos3,
+    fructos3: item.fructos3,
+    polyols3: item.polyols3,
+    lactos3: item.lactos3
   }
-
   fodMapsARR.push(fodmapObj3);
+  // return the array for the put or post methods to add to their req.body
+  return fodMapsARR
+}
 
-
-  // // req.body.fodmaps = fodMapsARR
-  req.body.fodmaps = fodMapsARR
-  console.log(req.body.fodmaps);
-  // need to add fodmaps same as posting
-  Food.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedItem) => {
-
-    res.redirect('/')
-  })
-})
-
-
-
+// create item
 router.post('/', (req, res) => {
-  let fodMapsARR = []
-
-  let fodmapObj1 = {
-    servingSizeW1: req.body.servingSizeW1,
-    servingSizeV1: req.body.servingSizeV1,
-    oligos1: req.body.oligos1,
-    fructos1: req.body.fructos1,
-    polyols1: req.body.polyols1,
-    lactos1: req.body.lactos1
-  }
-  fodMapsARR.push(fodmapObj1);
-
-  let fodmapObj2 = {
-    servingSizeW2: req.body.servingSizeW2,
-    servingSizeV2: req.body.servingSizeV2,
-    oligos2: req.body.oligos2,
-    fructos2: req.body.fructos2,
-    polyols2: req.body.polyols2,
-    lactos2: req.body.lactos2
-  }
-  fodMapsARR.push(fodmapObj2);
-
-  let fodmapObj3 = {
-    servingSizeW3: req.body.servingSizeW3,
-    servingSizeV3: req.body.servingSizeV3,
-    oligos3: req.body.oligos3,
-    fructos3: req.body.fructos3,
-    polyols3: req.body.polyols3,
-    lactos3: req.body.lactos3
-  }
-
-  fodMapsARR.push(fodmapObj3);
-
-
-  // req.body.fodmaps = fodMapsARR
-  req.body.fodmaps = fodMapsARR
-  // console.log(req.body.fodmaps);
-
+  req.body.fodmaps = makeFodMapsArr(req.body)
   Food.create(req.body, (err, newFood) => {
-    // console.log('new food: ' + newFood);
-
     res.redirect('/')
   })
 })
 
+// update item
+router.put('/:id', (req, res) => {
+  // send req.body to make fodmaps arry and then assign to req.body.fodmaps
+  req.body.fodmaps = makeFodMapsArr(req.body)
+  Food.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedItem) => {
+    res.redirect('/')
+  })
+})
+
+// delete item
 router.delete('/:id', (req, res) => {
   Food.findByIdAndRemove(req.params.id, (err, selectedItem) => {
     res.redirect('/')
