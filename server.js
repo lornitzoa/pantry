2bfeba933b8c9a0851bdf5ac78a872d6d9480e25
Anna Lornitzo <Anna@Annas-Mac.local>
2019-01-30 19:28:53 -0500

    reconfiguring for heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 //Configuration
 const PORT = process.env.PORT || 3000;
-const mongoURI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/pantry'
 
 // Middleware
 app.use(express.urlencoded({extended:false}));
@@ -46,10 +46,18 @@ app.use(session({
 // }))
 
 // Database
-mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
 mongoose.connection.once('open', () => {
   console.log('connected to mongo')
 })
+
 
 // Routes
 app.get('/', (req, res) => {
